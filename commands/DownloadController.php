dcdd1f02809eed256e34bca87b4bab220375b52f
sunchenwei <sun.chenwei@puscene.com>
2019-02-28 16:54:21 +0800

    下载网络文件

--- commands/DownloadController.php
+++ commands/DownloadController.php
@@ -20,10 +20,13 @@ class DownloadController extends Controller
         header("Content-type:text/html;charset=utf-8");
 
         $filepath = \Yii::$app->basePath.'/runtime/fanren/';
-        for($i=711;$i<1700;$i++){
-            $file="http://tingmp3.meiwenfen.com/%E7%8E%84%E5%B9%BB%E5%A5%87%E5%B9%BB/%E5%87%A1%E4%BA%BA%E4%BF%AE%E4%BB%99%E4%BC%A0/{$i}.mp3";
+        for($i=1;$i<1631;$i++){
+            $aim = $i;
+            if( $i<100 ) $aim = sprintf('%03d',$i);
+            $file="http://tingmp3.meiwenfen.com/%E7%8E%84%E5%B9%BB%E5%A5%87%E5%B9%BB/%E5%87%A1%E4%BA%BA%E4%BF%AE%E4%BB%99%E4%BC%A0/{$aim}.mp3";
+
             $handle = fopen($file,'r');
-            $filepathMp3 = $filepath."/{$i}.mp3";
+            $filepathMp3 = $filepath."/{$aim}.mp3";
             if( !file_exists($filepathMp3) ){
                 $handle1 = fopen($filepathMp3, "w");
                 fclose($handle1);
@@ -31,7 +34,7 @@ class DownloadController extends Controller
 
             $r = file_put_contents($filepathMp3, $handle);
             fclose($handle);
-            var_dump($i." $r \r\n");
+            var_dump($aim." $r $file \r\n");
             sleep(2);
         }
     }
